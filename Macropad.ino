60777dc8b99331573629b29d6df04b8108848a8a
nf61 <92657943+nf61@users.noreply.github.com>
2023-07-03 09:39:38 -0400

    Update Macropad.ino

--- Macropad.ino
+++ Macropad.ino
@@ -6,23 +6,11 @@
 
 //How many rows, columns, and LEDS have been wired
 #define WIDTH 4
-#define LED 4
-#define ROTPINS 3
-
-//how sensitive rotary encoder is higher the number the less sensitive
-#define ROT 3 
 
 //Pin numbers
-byte const leds [LED] = {16,18,14,15}; 
+
 byte const rowZero [WIDTH] = {21, 19, 10, 0};   //21,19,10,0
 byte const rowOne [WIDTH] = {20, 12, 2, 1};     //20,12,2,1
-byte const rotEnc [ROTPINS] = {6,7,8};
-
-//Speed starting leds go for lower the faster
-int ledSpeed = 400;
-
-//How many times LEDS cycle
-int cycles = 2;
 
 //Bounce objects set how sensitive the buttons are higher the number the less sensitive
 Bounce one = Bounce(rowZero[0], 10);
@@ -34,23 +22,13 @@ Bounce six = Bounce(rowOne[1], 10);
 Bounce seven = Bounce(rowOne[2], 10);
 Bounce eight = Bounce(rowOne[3], 10);
 
-//rotary encoder
-Bounce buton = Bounce(rotEnc[0], 10);
-Encoder knob(rotEnc[1], rotEnc[2]);
-long knobFormerState = knob.read();
-bool knobLayer = true;
-
 //number of layers
 byte layer = 0;
 byte pause = 0;
 
 void setup() {
 
-  //intialize led pins
-for(byte i = 0; i < LED; i++){
-  pinMode(leds[i], OUTPUT);
-  }
-  
+ 
   //intialize row pins
 for(byte i = 0; i < WIDTH; i++){
   pinMode(rowZero[i], INPUT_PULLUP);
@@ -63,31 +41,12 @@ for(byte i = 0; i < WIDTH; i++){
   digitalWrite(rowOne[i], HIGH);
   }  
   
-  //initiate rotary encoder
-for(byte i = 0; i < ROTPINS; i++){
-  pinMode(rotEnc[i], INPUT_PULLUP);
-  digitalWrite(rotEnc[i], HIGH);
-  }
-  
-  //Cycle leds
-for(int i = 0; i < cycles; i++){
-  for(byte j = 0; j < LED; j++){
-    digitalWrite(leds[j], HIGH);
-    delay(ledSpeed);
-    digitalWrite(leds[j],LOW);
-    }
-  }
-  
    //initiate keyboard
 Keyboard.begin();
 
 }
 
 void loop() {
-
-digitalWrite(leds[layer], HIGH);
-digitalWrite(leds[layer], LOW);
-
    
  // update buttons dont add long delays in loop
 one.update();
@@ -98,10 +57,6 @@ five.update();
 six.update();
 seven.update();
 eight.update();
-buton.update();
-
-//initiate rotary encoder
-long knobCurrentState = knob.read();
 
 // keycodes at https://www.pjrc.com/teensy/td_keyboard.html
 // if one doesn't work check to see if the error message gives the hex code like crtl above. 
@@ -226,61 +181,4 @@ if(eight.fallingEdge()) {
   }
  }
   
-if(buton.fallingEdge()) {
-  if(layer == 0){
-    if(knobLayer == true){
-      knobLayer = false;
-      }
-    else knobLayer = true;
-  }
 
-   if(layer == 1){
-      Keyboard.press(KEY_MEDIA_PLAY_PAUSE);
-      Keyboard.release(KEY_MEDIA_PLAY_PAUSE);
-     }
-}
-
-if(knobFormerState + ROT < knobCurrentState) {
-  if(layer == 0){
-    if(knobLayer == true){
-      Keyboard.press(KEY_UP);
-      Keyboard.release(KEY_UP);
-      knobFormerState = knobCurrentState;
-    }
-    else {
-      Keyboard.press(KEY_RIGHT);
-      Keyboard.release(KEY_RIGHT);
-      knobFormerState = knobCurrentState;
-    }
-  }
-
-  if(layer == 1){
-    Keyboard.press(KEY_MEDIA_VOLUME_INC);
-    Keyboard.release(KEY_MEDIA_VOLUME_INC);
-    knobFormerState = knobCurrentState;   
-  }
-}
-   
-if(knobFormerState - ROT > knobCurrentState) {
-  if(layer == 0){
-    if(knobLayer == true){
-      Keyboard.press(KEY_DOWN);
-      Keyboard.release(KEY_DOWN);
-      knobFormerState = knobCurrentState;
-    }
-    else {
-      Keyboard.press(KEY_LEFT);
-      Keyboard.release(KEY_LEFT);
-      knobFormerState = knobCurrentState;
-    }
-  }
-
-  if(layer == 1){
-    Keyboard.press(KEY_MEDIA_VOLUME_DEC);
-    Keyboard.release(KEY_MEDIA_VOLUME_DEC);
-    knobFormerState = knobCurrentState;    
-  }
-  }
-   
-   
-}
